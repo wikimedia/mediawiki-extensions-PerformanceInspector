a88775e2457460b32b58d3ee30f234e513b24e66
Reedy <reedy@wikimedia.org>
2017-03-07 22:10:30 +0000

    Use consistent array styles
    Fix indenting
    
    Remove unused variables
    
    Change-Id: I7e899a46982e70f24d7a121b479ae0987caf20c4

--- PerformanceInspector.hooks.php
+++ PerformanceInspector.hooks.php
@@ -8,34 +8,29 @@
 
 class PerformanceInspectorHooks {
 	public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
-
-		$action = $out->getRequest()->getVal( 'action' );
-		$isEdit = ( $action === 'edit' );
 		$title = $out->getTitle();
-		if ( $title->inNamespace( NS_MAIN ) /*&& $isEdit*/ ) {
+		if ( $title->inNamespace( NS_MAIN ) ) {
 			$out->addModuleStyles( [ 'ext.PerformanceInspector.noscript' ] );
-			$out->addModules( array( 'ext.PerformanceInspector.startup' ) );
+			$out->addModules( [ 'ext.PerformanceInspector.startup' ] );
 		}
-		return true;
 	}
 
 	public static function onBaseTemplateToolbox( BaseTemplate $baseTemplate, array &$toolbox ) {
-			$toolbox['performanceinspector'] = array(
-				'text' => $baseTemplate->getMsg( 'performanceinspector-portlet-link' )->text(),
-				'href' => '#',
-				'id' => 't-performanceinspector'
-			);
-		return true;
+		$toolbox['performanceinspector'] = [
+			'text' => $baseTemplate->getMsg( 'performanceinspector-portlet-link' )->text(),
+			'href' => '#',
+			'id' => 't-performanceinspector'
+		];
 	}
 
 	public static function onResourceLoaderTestModules( array &$testModules,
-		ResourceLoader &$resourceLoader ) {
-		$testModules['qunit']['ext.performanceInspector.tests'] = array(
-			'scripts' => array( 'tests/qunit/ext.performanceInspector.test.js' ),
-			'dependencies' => array( 'ext.PerformanceInspector.analyze' ),
+		ResourceLoader &$resourceLoader
+	) {
+		$testModules['qunit']['ext.performanceInspector.tests'] = [
+			'scripts' => [ 'tests/qunit/ext.performanceInspector.test.js' ],
+			'dependencies' => [ 'ext.PerformanceInspector.analyze' ],
 			'localBasePath' => __DIR__,
 			'remoteExtPath' => 'PerformanceInspector'
-		);
-		return true;
+		];
 	}
 }
