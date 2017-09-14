9ffa0ea4c5e15ca461fc8b4e121b851d1004e683
Gilles Dubuc <gilles@wikimedia.org>
2017-09-14 12:46:17 +0200

    Turn into a beta feature
    Bug: T146045
    Change-Id: I2d260282fd7e0f13f377919691a9e2d1be4f3a3e

--- PerformanceInspector.hooks.php
+++ PerformanceInspector.hooks.php
@@ -7,20 +7,36 @@
  */
 
 class PerformanceInspectorHooks {
+	/**
+	 * @param User $user
+	 *
+	 * @return bool
+	 */
+	private static function isBetaFeatureEnabled( User $user ) {
+		return class_exists( 'BetaFeatures' )
+			&& BetaFeatures::isFeatureEnabled( $user, 'performanceinspector' );
+	}
+
 	public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
 		$title = $out->getTitle();
-		if ( $title->inNamespace( NS_MAIN ) ) {
+		$user = $out->getUser();
+
+		if ( $title->inNamespace( NS_MAIN ) && self::isBetaFeatureEnabled( $user ) ) {
 			$out->addModuleStyles( [ 'ext.PerformanceInspector.noscript' ] );
 			$out->addModules( [ 'ext.PerformanceInspector.startup' ] );
 		}
 	}
 
 	public static function onBaseTemplateToolbox( BaseTemplate $baseTemplate, array &$toolbox ) {
-		$toolbox['performanceinspector'] = [
-			'text' => $baseTemplate->getMsg( 'performanceinspector-portlet-link' )->text(),
-			'href' => '#',
-			'id' => 't-performanceinspector'
-		];
+		$user = RequestContext::getMain()->getUser();
+
+		if ( self::isBetaFeatureEnabled( $user ) ) {
+			$toolbox['performanceinspector'] = [
+				'text' => $baseTemplate->getMsg( 'performanceinspector-portlet-link' )->text(),
+				'href' => '#',
+				'id' => 't-performanceinspector'
+			];
+		}
 	}
 
 	public static function onResourceLoaderTestModules( array &$testModules,
@@ -32,5 +48,30 @@ class PerformanceInspectorHooks {
 			'localBasePath' => __DIR__,
 			'remoteExtPath' => 'PerformanceInspector'
 		];
+	}
+
+	/**
+	 * Handler for the GetBetaFeaturePreferences hook.
+	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/GetBetaFeaturePreferences
+	 *
+	 * @param User $user User to get preferences for
+	 * @param array &$preferences Preferences array
+	 *
+	 * @return bool true in all cases
+	 */
+	public static function onGetBetaFeaturePreferences( User $user, array &$preferences ) {
+		$preferences['performanceinspector'] = [
+			'label-message' => 'performanceinspector-beta',
+			'desc-message' => 'performanceinspector-beta-desc',
+			'info-link' => 'https://www.mediawiki.org/wiki/Special:MyLanguage/' .
+							'Extension:PerformanceInspector',
+			'discussion-link' => 'https://www.mediawiki.org/wiki/' .
+								'Extension_talk:PerformanceInspector',
+			'requirements' => [
+				'javascript' => true,
+			]
+		];
+
+		return true;
 	}
 }
